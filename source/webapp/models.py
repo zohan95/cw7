c7bc6a545605be8a2f51e8e1779d3eb031fecda8
Ваше Имя <you@example.com>
2019-10-19 11:58:27 +0600

    Завершен этап 1

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Poll(models.Model):
+    question = models.TextField(max_length=1000, verbose_name='Вопрос')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.question[:20]
+
+
+class Choice(models.Model):
+    variation = models.TextField(max_length=200, verbose_name='Текст варианта')
+    poll = models.ForeignKey('webapp.Poll', verbose_name='Вопрос', related_name='poll', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.variation[:20]
+
+
